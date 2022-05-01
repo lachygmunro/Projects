cf2e8988411e56031a96c35cbea46b3617ead616
Lachy Munro <43901618+lachygmunro@users.noreply.github.com>
2022-05-01 22:59:09 +1000

    Duplicate
    Renamed project. This is the previous now duplicate directory.

--- LandingPage/index.js
+++
@@ -1,7 +0,0 @@
-//mobile menu
-const burgerMenu = document.querySelector('#mob-burger')
-const navbarMenu = document.querySelector('#nav-links')
-
-burgerMenu.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active')
-})
